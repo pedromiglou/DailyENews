acbd79f970709231ab89a94be385a934db268259
Pedro Amaral <pedromiglou@hotmail.com>
2022-05-26 11:32:19 +0100

    simplify server and worker dockerfile

--- jarr/start-server.sh
+++ jarr/start-server.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
+# create database tables
 pipenv run flask bootstrap-database
 
+# run flask
 pipenv run gunicorn -c /etc/jarr/gunicorn.py --log-config /etc/jarr/logging.ini -b 0.0.0.0:8000 wsgi:application
